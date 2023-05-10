c41b9f0a3ba2b7d429dd7f15ea04e15076538a4e
Erzh 08 <raul.silva36@fatec.sp.gov.br>
2023-05-09 22:05:05 -0300

    Alterar pacotes utilizados

---
+++ lib/src/constants.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+// Text
+const String appName = 'Green Style';
+const String apiUrl = '';
+
+// Colors
+const Color lightBackgroundColor = Colors.white;
+const Color darkBackgroundColor = Color.fromARGB(255, 5, 28, 62);
+const Color buttonBackgroundColor = Color.fromARGB(255, 51, 163, 96);
+const Color activebuttonBackgroundColor = Color.fromARGB(255, 34, 108, 64);
+
+final greenStyleTheme = ThemeData(
+  scaffoldBackgroundColor: lightBackgroundColor,
+  primarySwatch: Colors.green,
+);
+
+// Complexer widgets
+class GreenStyleBottomNavigationBar extends StatelessWidget {
+  @override Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(
+          icon: FaIcon(FontAwesomeIcons.house),
+          label: ''
+        ),
+        BottomNavigationBarItem(
+          icon: FaIcon(FontAwesomeIcons.chartPie),
+          label: ''
+        ),
+        BottomNavigationBarItem(
+          icon: FaIcon(FontAwesomeIcons.clipboardQuestion),
+          label: ''
+        ),
+        BottomNavigationBarItem(
+          icon: FaIcon(FontAwesomeIcons.gear),
+          label: ''
+        ),
+      ],
+      selectedItemColor: buttonBackgroundColor,
+      onTap: (index) {
+        switch (index) {
+          case 0:
+            Navigator.of(context).pushReplacementNamed(
+              '/'
+            );
+            break;
+          case 1:
+            Navigator.of(context).pushReplacementNamed(
+              '/compare'
+            );
+            break;
+          case 2:
+            Navigator.of(context).pushReplacementNamed(
+              '/quiz'
+            );
+            break;
+          case 3:
+            Navigator.of(context).pushReplacementNamed(
+              '/account'
+            );
+            break;
+        }
+      },
+    );
+  }
+}
