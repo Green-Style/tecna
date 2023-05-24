f4858bb1ac1b5c481e48e768b7cf29813c098568
Erzh 08 <raul.silva36@fatec.sp.gov.br>
2023-05-23 21:50:38 -0300

    Alterar totalmente forma de gerar gráfico

--- lib/src/view/comparison_chart.dart
+++ lib/src/view/comparison_chart.dart
@@ -1,8 +1,10 @@
+import 'dart:math' as math;
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:green_style/src/constants.dart';
 import 'package:green_style/src/controller/home_controller.dart';
+import 'package:green_style/src/model/comparison.dart';
 
 class ComparisonChart extends StatelessWidget {
   ComparisonChart({super.key});
@@ -13,7 +15,7 @@ class ComparisonChart extends StatelessWidget {
     return await storage.read(key: userTokenKey);
   }
 
-  Future<BarChartData> _getComparisonChartData() async {
+  Future<Comparison> _getComparisonChartData() async {
     final data = await homeCtrl.getComparisonChartData(await getUserToken());
 
     return data;
@@ -31,27 +33,28 @@ class ComparisonChart extends StatelessWidget {
             if (snapshot.hasError) {
               return Text("Error: ${snapshot.error}");
             } else {
-              return SingleChildScrollView(
-                child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: darkBackgroundColor,
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Center(
-                        child: Text( // TODO: Alterar para sugestão buscada da API
-                          'algo',
-                          style: TextStyle(
-                            color: Colors.white
-                          ),
+              return DecoratedBox(
+                decoration: BoxDecoration(
+                  color: darkBackgroundColor
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Center(
+                      child: Text( // TODO: Alterar para sugestão buscada da API
+                        'algo',
+                        style: TextStyle(
+                          color: Colors.white
                         ),
                       ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      DecoratedBox( // Legendas
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Expanded(
+                      child: DecoratedBox( // Legendas
                         decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: const BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
@@ -59,18 +62,28 @@ class ComparisonChart extends StatelessWidget {
                             style: BorderStyle.none
                           )
                         ),
-                        child: SizedBox(
-                          height: 250,
-                          width: double.infinity,
-                          child: Center(
-                            child: BarChart(
-                              snapshot.data!
+                        child: Align(
+                          alignment: FractionalOffset.center,
+                          child: SizedBox(
+                            height: 250,
+                            width: double.infinity,
+                            child: Center(
+                              child: Column(
+                                children: [
+                                  Co2BarChart(dataList: snapshot.data!.dataList),
+                                  Row(
+                                    children: [
+
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    ),
+                  ],
                 ),
               );
             }
@@ -78,4 +91,224 @@ class ComparisonChart extends StatelessWidget {
       },
     );
   }
+}
+
+class Co2BarChart extends StatefulWidget {
+  const Co2BarChart({super.key, required this.dataList});
+  final List<BarData> dataList;
+  final shadowColor = const Color(0xFFCCCCCC);
+
+  @override
+  State<Co2BarChart> createState() => _Co2BarChartState();
+}
+
+class _Co2BarChartState extends State<Co2BarChart> {
+  BarChartGroupData generateBarGroup(
+    int x,
+    Color color,
+    double value,
+  ) {
+    return BarChartGroupData(
+      x: x,
+      barRods: [
+        BarChartRodData(
+          toY: value,
+          color: color,
+          width: 28,
+        ),
+      ],
+      showingTooltipIndicators: touchedGroupIndex == x ? [0] : [],
+    );
+  }
+
+  int touchedGroupIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: AspectRatio(
+        aspectRatio: 1.4,
+        child: BarChart(
+          BarChartData(
+            alignment: BarChartAlignment.spaceBetween,
+            borderData: FlBorderData(
+              show: true,
+              border: Border.symmetric(
+                horizontal: BorderSide(
+                  color: AppColors.borderColor.withOpacity(0.2),
+                ),
+              ),
+            ),
+            titlesData: FlTitlesData(
+              show: true,
+              leftTitles: AxisTitles(
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  reservedSize: 30,
+                  getTitlesWidget: (value, meta) {
+                    return Text(
+                      value.toInt().toString(),
+                      textAlign: TextAlign.left,
+                    );
+                  },
+                ),
+              ),
+              bottomTitles: AxisTitles(
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  reservedSize: 36,
+                  getTitlesWidget: (value, meta) {
+                    final index = value.toInt();
+                    return SideTitleWidget(
+                      axisSide: meta.axisSide,
+                      child: _IconWidget(
+                        color: widget.dataList[index].color,
+                        isSelected: touchedGroupIndex == index,
+                      ),
+                    );
+                  },
+                ),
+              ),
+              rightTitles: AxisTitles(),
+              topTitles: AxisTitles(),
+            ),
+            gridData: FlGridData(
+              show: true,
+              drawVerticalLine: false,
+              getDrawingHorizontalLine: (value) => FlLine(
+                color: AppColors.borderColor.withOpacity(0.2),
+                strokeWidth: 1,
+              ),
+            ),
+            barGroups: widget.dataList.asMap().entries.map((e) {
+              final index = e.key;
+              final data = e.value;
+              return generateBarGroup(
+                index,
+                data.color,
+                data.value,
+              );
+            }).toList(),
+            barTouchData: BarTouchData(
+              enabled: true,
+              handleBuiltInTouches: false,
+              touchTooltipData: BarTouchTooltipData(
+                tooltipBgColor: Colors.transparent,
+                tooltipMargin: 0,
+                getTooltipItem: (
+                  BarChartGroupData group,
+                  int groupIndex,
+                  BarChartRodData rod,
+                  int rodIndex,
+                ) {
+                  return BarTooltipItem(
+                    rod.toY.toString(),
+                    TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: rod.color,
+                      fontSize: 18,
+                      shadows: const [
+                        Shadow(
+                          color: Colors.black26,
+                          blurRadius: 12,
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+              touchCallback: (event, response) {
+                if (event.isInterestedForInteractions &&
+                    response != null &&
+                    response.spot != null) {
+                  setState(() {
+                    touchedGroupIndex = response.spot!.touchedBarGroupIndex;
+                  });
+                } else {
+                  setState(() {
+                    touchedGroupIndex = -1;
+                  });
+                }
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BarData {
+  const BarData(this.color, this.value);
+  final Color color;
+  final double value;
+}
+
+class _IconWidget extends ImplicitlyAnimatedWidget {
+  const _IconWidget({
+    required this.color,
+    required this.isSelected,
+  }) : super(duration: const Duration(milliseconds: 300));
+  final Color color;
+  final bool isSelected;
+
+  @override
+  ImplicitlyAnimatedWidgetState<ImplicitlyAnimatedWidget> createState() =>
+      _IconWidgetState();
+}
+
+class _IconWidgetState extends AnimatedWidgetBaseState<_IconWidget> {
+  Tween<double>? _rotationTween;
+
+  @override
+  Widget build(BuildContext context) {
+    final rotation = math.pi * 4 * _rotationTween!.evaluate(animation);
+    final scale = 1 + _rotationTween!.evaluate(animation) * 0.5;
+    return Transform(
+      transform: Matrix4.rotationZ(rotation).scaled(scale, scale),
+      origin: const Offset(14, 14),
+      child: Icon(
+        widget.isSelected ? Icons.cloud : Icons.cloud_outlined,
+        color: widget.color,
+        size: 48,
+      ),
+    );
+  }
+
+  @override
+  void forEachTween(TweenVisitor<dynamic> visitor) {
+    _rotationTween = visitor(
+      _rotationTween,
+      widget.isSelected ? 1.0 : 0.0,
+      (dynamic value) => Tween<double>(
+        begin: value as double,
+        end: widget.isSelected ? 1.0 : 0.0,
+      ),
+    ) as Tween<double>?;
+  }
+}
+
+class AppColors {
+  static const Color primary = contentColorCyan;
+  static const Color menuBackground = Color(0xFF090912);
+  static const Color itemsBackground = Color(0xFF1B2339);
+  static const Color pageBackground = Color(0xFF282E45);
+  static const Color mainTextColor1 = Colors.white;
+  static const Color mainTextColor2 = Colors.white70;
+  static const Color mainTextColor3 = Colors.white38;
+  static const Color mainGridLineColor = Colors.white10;
+  static const Color borderColor = Colors.white54;
+  static const Color gridLinesColor = Color(0x11FFFFFF);
+
+  static const Color contentColorBlack = Colors.black;
+  static const Color contentColorWhite = Colors.white;
+  static const Color contentColorBlue = Color(0xFF2196F3);
+  static const Color contentColorYellow = Color(0xFFFFC300);
+  static const Color contentColorOrange = Color(0xFFFF683B);
+  static const Color contentColorGreen = Color(0xFF3BFF49);
+  static const Color contentColorPurple = Color(0xFF6E1BFF);
+  static const Color contentColorPink = Color(0xFFFF3AF2);
+  static const Color contentColorRed = Color(0xFFE80054);
+  static const Color contentColorCyan = Color(0xFF50E4FF);
 }
