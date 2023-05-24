684f008c0f3d276d0c45170a35be3e32252278be
Erzh 08 <raul.silva36@fatec.sp.gov.br>
2023-05-23 21:50:09 -0300

    Criar Comparison

---
+++ lib/src/model/comparison.dart
@@ -0,0 +1,11 @@
+import 'package:green_style/src/view/comparison_chart.dart';
+
+class Comparison {
+  List<BarData> dataList;
+  List<String> labels;
+
+  Comparison({
+    required this.dataList,
+    required this.labels,
+  });
+}
