e931c1f93a017f9326a6482d4ac428468d9f8069
Erzh 08 <raul.silva36@fatec.sp.gov.br>
2023-05-26 22:57:23 -0300

    Inclusão de novas cores

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -8,6 +8,7 @@ const String userTokenKey = 'greenStyleUserToken';
 
 // Colors
 const Color lightBackgroundColor = Colors.white;
+const Color borderColor = Colors.black;
 const Color darkBackgroundColor = Color.fromARGB(255, 5, 28, 62);
 const Color buttonBackgroundColor = Color.fromARGB(255, 51, 163, 96);
 const Color activebuttonBackgroundColor = Color.fromARGB(255, 34, 108, 64);
@@ -17,6 +18,9 @@ const Color foodCategoryColor = Color.fromARGB(255, 145, 5, 54);
 const Color electricityCategoryColor = Color.fromARGB(255, 8, 144, 198);
 const Color purchaseCategoryColor = Color.fromARGB(255, 103, 245, 21);
 const Color transportationCategoryColor = Color.fromARGB(255, 63, 2, 155);
+const Color initialEmissionColor = Color.fromARGB(255, 8, 144, 198);
+const Color actualEmissionColor = Color.fromARGB(255, 103, 245, 21);
+const Color globalEmissionColor = Color.fromARGB(255, 63, 2, 155);
 
 final greenStyleTheme = ThemeData(
   scaffoldBackgroundColor: lightBackgroundColor,
