5bf1f6de13373b3e01c5d5a740672e3642e7d4ea
Erzh 08 <raul.silva36@fatec.sp.gov.br>
2023-05-31 17:16:17 -0300

    Incluir texto de comparação no model

--- lib/src/model/comparison.dart
+++ lib/src/model/comparison.dart
@@ -3,9 +3,11 @@ import 'package:green_style/src/view/comparison_chart.dart';
 class Comparison {
   List<BarData> dataList;
   List<String> labels;
+  String comparison;
 
   Comparison({
     required this.dataList,
     required this.labels,
+    required this.comparison,
   });
 }
